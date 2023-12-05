0b8fac4cf9ecf7ecec92518a06b275084fba340a
devillies <devilliesdev@gmail.com>
2023-12-05 20:26:21 +0800

    changed import export and add index as main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import Phaser from "phaser";
-import config from "./config";
-import GameScene from "./scenes/Game";
+import { AUTO, Scale, Game } from 'phaser'
+import AmongUsScene from './scenes/AmongUsScene'
+
+const config = {
+	type: AUTO,
+	backgroundColor: '#32A5E7',
+	scale: {
+		width: 1920,
+		height: 1080,
+		mode: Scale.FIT,
+		autoCenter: Scale.CENTER_BOTH,
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: -1 },
+			debug: false,
+		},
+	},
+	scene: [AmongUsScene],
+}
 
 /* eslint-disable-next-line */
-const game = new Phaser.Game(Object.assign(config, { scene: [GameScene] }));
+export default new Game(config)
